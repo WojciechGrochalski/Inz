2be9fad83ecf06fc0d084b8c5055bc6aed4480f9
WojciechGrochalski <alchemik789@gmail.com>
2021-02-28 19:01:17 +0100

    Reduce image programing-env2

--- programing-env/dotnet.sh
+++ programing-env/dotnet.sh
@@ -8,15 +8,13 @@
 #!/bin/bash 
 if ${Dotnet} 
 then
-   echo "Installation packages for dotnet"
    pkg=dotnet-sdk-3.1
    status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
-   echo $status
    if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
       echo "package $pkg will be install on background"
    else echo "package $pkg was installed previously"
       dotnet --info
    fi
    
-else echo "No flags dotnet"
+else echo "No flag dotnet"
 fi
