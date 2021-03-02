1ab39e52796957e2fdafc56544018f55ea359a22
WojciechGrochalski <alchemik789@gmail.com>
2021-03-02 13:21:12 +0100

    speedtest update

--- speedtest/script.py
+++ speedtest/script.py
@@ -6,12 +6,9 @@ import time
 import speedtest
 import threading
 import json
-#def run():
 
-print("Start testing")
 
-while True :
-    #os.system(f"sed -i 's/Configuration Overview/start/g' /var/www/html/speedtest/index.html")
+def st():
     st = speedtest.Speedtest()
     st.get_best_server()
     res = st.get_best_server()
@@ -29,22 +26,17 @@ while True :
   
     print(st.results.ping)
     data={"downloads":downloads, "uploads": upload, "ping": int(st.results.ping)}
-    with open('/var/www/html/speedtest/data.json', 'w') as outfile:
-        json.dump(data, outfile)
-    exit=input('$:')
-    if(exit=='exit'):
-        break
+
+    #with open('/var/www/html/speedtest/data.json', 'w') as outfile:
+    file = open('/var/www/html/speedtest/data.json', 'w')
+    json.dump(data,file)
+    file.close()
+       
+    print("done")
+print("Start testing")
+while True :
+    st()
+    time.sleep(20)
 print("Exiting program")
 
-# thread = threading.Thread(target=run, args=())
-# thread.daemon = True
-# thread.start()
 
-#   Html_file= open("/var/www/html/speedtest/index.html","r")
-#     html_str=Html_file.read()
-#     Html_file.close()
-#     html_str = html_str.replace(html, "<div id=""dlText"" class=""meterText"">{downloads}</div>")
- 
-#     Html_file= open("/var/www/html/speedtest/index.html","w")
-#     Html_file.write(html_str)
-#     Html_file.close()
--- speedtest/startup.sh
+++ speedtest/startup.sh
@@ -1,5 +1,7 @@
 
 echo "start Apache2 service"
 sed -i 's/It works!/It works! from Container/g' /var/www/html/index.html
-/usr/sbin/apache2ctl -D FOREGROUND && bash
+# /usr/sbin/apache2ctl -D FOREGROUND && bash
+service apache2 start
 python script.py
+
