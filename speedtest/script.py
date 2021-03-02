eec70ca3b54af66eb36a7f9ac60ac25f733864f2
WojciechGrochalski <alchemik789@gmail.com>
2021-03-02 20:12:06 +0100

    update speedtest html

--- speedtest/script.py
+++ speedtest/script.py
@@ -6,37 +6,41 @@ import time
 import speedtest
 import threading
 import json
-
+       
+interval = int(os.environ['interval'])
 
 def st():
-    st = speedtest.Speedtest()
-    st.get_best_server()
-    res = st.get_best_server()
-    print('0) Server location: ',end='')
-    print(res["name"])
-    print('1) Download Speed: ',end='')
-    downloads=round(st.download()/1000000,1)
-    print(downloads, "Mbps")
-   
-    print('1) Upload Speed: ', end='')
-    upload=round(st.upload()/1000000,1)
-    print(upload, "Mbps")
-   
-    print('1) Ping: ', end='')
-  
-    print(st.results.ping)
-    data={"downloads":downloads, "uploads": upload, "ping": int(st.results.ping)}
+    try:
+        st = speedtest.Speedtest()
+        st.get_best_server()
+        res = st.get_best_server()
+        print('0) Server location: ',end='')
+        print(res["name"])
+        print('1) Download Speed: ',end='')
+        downloads=round(st.download()/1000000,1)
+        print(downloads, "Mbps")
+    
+        print('1) Upload Speed: ', end='')
+        upload=round(st.upload()/1000000,1)
+        print(upload, "Mbps")
+    
+        print('1) Ping: ', end='')
+    
+        print(st.results.ping)
+        data={"downloads":downloads, "uploads": upload, "ping": int(st.results.ping)}
 
-    #with open('/var/www/html/speedtest/data.json', 'w') as outfile:
-    file = open('/var/www/html/speedtest/data.json', 'w')
-    json.dump(data,file)
-    file.close()
-       
-    print("done")
+        file = open('/var/www/html/speedtest/data.json', 'w')
+        json.dump(data,file)
+        file.close()
+        
+        print("done")
+    except Exception as e:
+        print(e)
 print("Start testing")
+
 while True :
     st()
-    time.sleep(20)
+    time.sleep(interval*60)
 print("Exiting program")
 
 
