4e99c05f079f235c9ea01a3081aa8df2628e5f12
WojciechGrochalski <alchemik789@gmail.com>
2021-03-05 11:09:05 +0100

    add ip to index.html

--- speedtest/script.py
+++ speedtest/script.py
@@ -3,10 +3,23 @@ import os
 import time
 import speedtest
 import json
-       
-interval = int(os.environ['interval'])
-
-def st():
+import socket
+try:
+    interval = int(os.environ['interval'])
+except:
+    interval=5
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+def st(ip):
     try:
         st = speedtest.Speedtest()
         st.get_best_server()
@@ -24,7 +37,7 @@ def st():
         print('1) Ping: ', end='')
     
         print(st.results.ping)
-        data={"downloads":downloads, "uploads": upload, "ping": int(st.results.ping)}
+        data={"downloads":downloads, "uploads": upload, "ping": int(st.results.ping), "ip":ip}
         with open('/var/www/html/speedtest/data.json', 'w') as outfile:
             json.dump(data, outfile)
         
@@ -32,9 +45,9 @@ def st():
     except Exception as e:
         print(e)
 print("Start testing")
-
+ip=get_ip()
 while True :
-    st()
+    st(ip)
     time.sleep(interval*60)
 print("Exiting program")
 
